209da665e84210a763b5348b3d6cd520a6eaae38
Joachim Hamraoui <joachimhamraoui@MacBook-Pro.local>
2025-03-13 21:25:45 +0100

    feat(listItems): added basic UI and logic for list and item components

---
+++ src/components/item.js
@@ -0,0 +1,10 @@
+const Item = ({ date, id, title }) => {
+  return (
+    <div id={id} className="bg-gray-100 px-4 py-2 rounded-md font-main">
+      <h1 className="font-bold text-xl">{title}</h1>
+      <p className="text-gray-500 text-sm">{date}</p>
+    </div>
+  );
+};
+
+export default Item;
--- src/components/listWrapper.js
+++ src/components/listWrapper.js
@@ -1,12 +1,23 @@
+import Item from "./item.js";
+import { useState } from "react";
+
 const ListWrapper = () => {
+  const [listItems, setListItems] = useState([]);
+
   return (
     <div>
       <h1 className="text-xl text-white font-main">To-Do List</h1>
       <div className="w-100 mt-4">
-        
+        <Item date="2023-01-01" id="1" title="Say hi to Mom 👋" />
+
+        <input
+          type="text"
+          placeholder="Add a new item"
+          className="mt-4 w-full bg-gray-100 px-4 py-2 rounded-md font-main text-black"
+        ></input>
       </div>
     </div>
   );
-}
+};
 
 export default ListWrapper;
