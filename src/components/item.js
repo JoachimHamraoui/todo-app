dabda596bf3f022e0e7111429d3dbc59e8db205b
Joachim Hamraoui <joachimhamraoui@MacBook-Pro.local>
2025-03-13 21:37:08 +0100

    feat(listItems): items can now be added through the text input and key press 'Enter' !

--- src/components/item.js
+++ src/components/item.js
@@ -1,6 +1,6 @@
 const Item = ({ date, id, title }) => {
   return (
-    <div id={id} className="bg-gray-100 px-4 py-2 rounded-md font-main">
+    <div id={id} className="bg-gray-100 px-4 py-2 rounded-md font-main mb-2">
       <h1 className="font-bold text-xl">{title}</h1>
       <p className="text-gray-500 text-sm">{date}</p>
     </div>
--- src/components/listWrapper.js
+++ src/components/listWrapper.js
@@ -3,17 +3,36 @@ import { useState } from "react";
 
 const ListWrapper = () => {
   const [listItems, setListItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const date = new Date().toDateString();
+
+  const addItem = () => {
+    setListItems([...listItems, inputValue]);
+    setInputValue("");
+  };
+
+  // when the enter key is pressed, add the item
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      addItem();
+    }
+  }
 
   return (
     <div>
       <h1 className="text-xl text-white font-main">To-Do List</h1>
       <div className="w-100 mt-4">
-        <Item date="2023-01-01" id="1" title="Say hi to Mom 👋" />
+        {listItems.map((item, index) => (
+          <Item key={index} date={date} id={index} title={item} />
+        ))}
 
         <input
           type="text"
           placeholder="Add a new item"
-          className="mt-4 w-full bg-gray-100 px-4 py-2 rounded-md font-main text-black"
+          className="w-full bg-gray-100 px-4 py-2 rounded-md font-main text-black"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyPress={handleKeyPress}
         ></input>
       </div>
     </div>
